11b6792c16d4133d7f5f4e8bd0409ac11828444a
iain <iain@picturk.com>
2014-01-30 17:07:30 +0000

    Added config for generators in the gem

--- lib/support_form/engine.rb
+++ lib/support_form/engine.rb
@@ -1,4 +1,9 @@
 module SupportForm
   class Engine < ::Rails::Engine
+    config.generators do |g|
+      g.orm             :active_record
+      g.template_engine :erb
+      g.test_framework  :rspec
+    end
   end
 end
