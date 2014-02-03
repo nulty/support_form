e9512ab4184bc482c20c2dfb480bc7524ba10014
iain <iain@picturk.com>
2014-02-03 14:09:44 +0000

    fixing the routing and namespacing for views and controllers

--- app/controllers/support_form/stats_controller.rb
+++ app/controllers/support_form/stats_controller.rb
@@ -1,7 +1,7 @@
-require 'pry'
 module SupportForm
   class StatsController < ApplicationController
     def index
+      @stats = SupportForm::Stat.all
     end
 
     def show
@@ -13,16 +13,26 @@ module SupportForm
     end
 
     def edit
+      @support_stats = SupportForm::Stat.find(params[:id])
     end
 
     def create
       @support_stats = SupportForm::Stat.create(params[:stat])
+      respond_to do |format|
+        format.html {redirect_to(root_url)}
+      end
     end
 
     def update
+      @stat = SupportForm::Stat.find(params[:id])
+      @stat.update_attributes(params[:stat])
+      redirect_to("/")
     end
 
     def destroy
+      @stat = SupportForm::Stat.find(params[:id])
+      @stat.destroy
+      redirect_to root_url
     end
   end
 end
--- spec/controllers/support_form/stats/stats_controller_spec.rb
+++ spec/controllers/support_form/stats/stats_controller_spec.rb
@@ -1,9 +1,10 @@
+require 'pry'
 require 'spec_helper'
 
 describe SupportForm::StatsController do
 
   before(:each) do
-    @stat = SupportForm::Stat.create
+    @stat = SupportForm::Stat.create(stats: {"category_1" => "4"}, recipient_email: "iain@picturk.com")
   end
 
   after(:each) do
@@ -15,33 +16,48 @@ describe SupportForm::StatsController do
       get 'index'
       response.should be_success
     end
+
+    it "assigns variable correctly" do
+      get 'index'
+      expect(assigns(:stats)).to match_array([@stat])
+    end
   end
 
   describe "GET 'show'" do
     it "returns http success" do
       get 'show', id: @stat
-      response.should be_success
+      expect(response).to be_success
     end
   end
 
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
-      response.should be_success
+      expect(response).to be_success
+    end
+
+    it "assigns a new stat" do
+      get 'new'
+      expect(assigns(:support_stats)).to be_a_new(SupportForm::Stat)
     end
   end
 
   describe "GET 'edit'" do
     it "returns http success" do
       get 'edit', id: @stat
-      response.should be_success
+      expect(response).to be_success
+    end
+
+    it "assigns a new stat" do
+      get 'edit', id: @stat
+      expect(assigns(:support_stats)).to eq @stat
     end
   end
 
   describe "POST 'create'" do
     it "returns http success" do
-      post 'create'
-      response.should be_success
+      post 'create', {stat: {stats: {"category_1" => "2"}, recipient_email: "iain@picturk.com"}}
+      expect(response).to be_redirect
     end
 
     it "creates a new Stat" do
@@ -52,16 +68,28 @@ describe SupportForm::StatsController do
   end
 
   describe "PUT 'update'" do
-    it "returns http success" do
-      put 'update', id: @stat
-      response.should be_success
+    it "changes the record" do
+      expect{
+        put 'update', {id: @stat, stat: { recipient_email: "new@picturk.com" }}
+        }.to change{ @stat.reload.recipient_email }.to("new@picturk.com").from("iain@picturk.com")
+    end
+
+    it "renders the page" do
+        put 'update', {id: @stat, stat: { recipient_email: "new@picturk.com" }}
+        expect(response).to be_redirect
     end
   end
 
   describe "DELETE 'destroy'" do
-    it "returns http success" do
+    it "destroys a record" do
+      expect{
+        delete 'destroy', id: @stat
+        }.to change{SupportForm::Stat.count}.by(-1)
+    end
+
+    it "redirects to root url" do
       delete 'destroy', id: @stat
-      response.should be_success
+      expect(response).to be_redirect
     end
   end
 
