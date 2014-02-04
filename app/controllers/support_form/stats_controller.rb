c18eb63d4b50edcca2b421b6cfff130a668c6b92
iain <iain@picturk.com>
2014-02-04 11:32:25 +0000

    Added support_form creator form and action

--- app/controllers/support_form/stats_controller.rb
+++ app/controllers/support_form/stats_controller.rb
@@ -17,9 +17,17 @@ module SupportForm
     end
 
     def create
-      @support_stats = SupportForm::Stat.create(params[:stat])
+      h = Hash["stats", {}, "recipient_email", ""]
+      if params["support_form_stat"]["category_name"].kind_of? Hash
+        h["stats"].merge!(params["support_form_stat"]["category_name"].invert)
+        h["stats"].each { |k,v| h["stats"][k] = 0 }
+      else
+        h["stats"][params["support_form_stat"]["category_name"]] = 0
+      end
+      h["recipient_email"] = params["support_form_stat"]["recipient_email"]
+      @support_stats = SupportForm::Stat.create(h)
       respond_to do |format|
-        format.html {redirect_to(root_url)}
+        format.html { redirect_to(root_url) }
       end
     end
 
