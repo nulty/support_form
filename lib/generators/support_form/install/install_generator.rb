4690b7b2f82f8a04512f6ba7a81b556ad6371032
iain <iain@picturk.com>
2014-02-10 18:52:26 +0000

    Fixed Add Category js generated html in stat form

--- lib/generators/support_form/install/install_generator.rb
+++ lib/generators/support_form/install/install_generator.rb
@@ -1,18 +1,12 @@
 require 'generators/support_form'
-require 'rubygems'
+
 module SupportForm
   module Generators
     class InstallGenerator < Base
-      desc "Create the necessary models and controllers for the application"
+      desc "Global install for support_form"
 
-      # source_root File.expand_path("../templates", __FILE__)
-      def run_scaffold_generator
+      def install_support_form
         # create_file "config/initializers/initializer.rb", "# Add initialization content here"
-
-        inject_into_file "app/helpers/application_helper.rb", after: "module ApplicationHelper\n" do <<-'RUBY'
-  include SupportForm::Enquiries::FormHelper
-        RUBY
-        end
       end
       invoke "support_form:migrate"
     end
