46b833ae7afb494a441ed634e2beb3fd91fb8757
iain <iain@picturk.com>
2014-03-06 11:34:08 +0000

    Adding stats form improvements

--- app/assets/javascripts/support_form/stats_form.js
+++ app/assets/javascripts/support_form/stats_form.js
@@ -6,6 +6,18 @@ $(document).ready(function() {
   name_id = $('[class=category_name]').first().attr("id")
   val_id = $('[class=category_value]').first().attr("id")
   count = $("[class=category_name]").length
+  form = "<div id='support-form'><table> \
+      <tbody><tr> \
+        <td> \
+        <input class='category_name' id='international_salon_salon_competition_support_stats_attributes_categories_0' name='international_salon_salon_competition[support_stats_attributes][categories][0]' size='30' type='text' value=''></td> \
+        <td><input class='category_value' id='international_salon_salon_competition_support_stats_attributes_category_value_0' name='international_salon_salon_competition[support_stats_attributes][category_value][0]' size='4' type='text' value='0'></td> \
+        <td><a href='#' class='delete_category'>Delete</a></td> \
+    </tr></tbody></table> \
+      <div> \
+          <a href='#' id='add_category'>Add a Category</a>  </div>  <div class='control-group'> \
+        <label for='international_salon_salon_competition_support_stats_attributes_recipient_email'>Recipient email</label> \
+    <input id='international_salon_salon_competition_support_stats_attributes_recipient_email' name='international_salon_salon_competition[support_stats_attributes][recipient_email]' size='30' type='email'> \
+        </div></div>"
 
   $("[class=category_name]").each(function(idx,ele){
     $(this).attr("name", name_name + "["+idx+"]");
@@ -40,4 +52,17 @@ $(document).ready(function() {
     $(this).parents("tr").remove();
   })
 
+  $('#no-support a').click(function(e) {
+    e.preventDefault();
+    $('div#support-form').empty();
+    $('#create-support').toggle();
+    $(this).toggle();
+  })
+
+  $('#create-support').click(function(e) {
+    e.preventDefault();
+    $('div#support-form').html(form);
+    $('div#no-support a').toggle();
+    $(this).toggle();
+  })
 })
