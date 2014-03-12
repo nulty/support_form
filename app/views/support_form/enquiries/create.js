f58386a4c3add9cae55011d9dbdf9816408d1258
iain <iain@picturk.com>
2014-03-12 15:02:18 +0000

    removed unneeded local variable passed to partial

--- app/views/support_form/enquiries/create.js
+++ app/views/support_form/enquiries/create.js
@@ -1,8 +1,8 @@
 <% if @result %>
-  $('#support-form').html("<%= escape_javascript(render('support_form/enquiries/form', {enquiry: @enquiry}))%>")
+  $('#support-form').html("<%= escape_javascript(render('support_form/enquiries/form'))%>")
     $('#support_form_enquiry_message, #support_form_enquiry_name, #support_form_enquiry_topic, #support_form_enquiry_email').each(function(idx,ele) {
     $(ele).val("");
   })
 <% else %>
-  $('#support-form').html("<%= escape_javascript(render('support_form/enquiries/form', {enquiry: @enquiry}))%>")
+  $('#support-form').html("<%= escape_javascript(render('support_form/enquiries/form'))%>")
 <% end %>
